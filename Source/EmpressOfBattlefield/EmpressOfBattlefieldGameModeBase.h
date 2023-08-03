0348ba2ccf646ecb1f114e7131960d05a65f9f63
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-08-03 21:55:12 +0300

    initial commit

---
+++ Source/EmpressOfBattlefield/EmpressOfBattlefieldGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EmpressOfBattlefieldGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EMPRESSOFBATTLEFIELD_API AEmpressOfBattlefieldGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
