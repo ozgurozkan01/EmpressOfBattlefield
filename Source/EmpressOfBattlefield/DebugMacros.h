883a5ad05bf6d87fd22dd2b65aea8c086480275f
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-08-03 22:52:45 +0300

    add custom header file

---
+++ Source/EmpressOfBattlefield/DebugMacros.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+
+#define DRAW_DEBUG_SPHERE(Location) if (GetWorld()) DrawDebugSphere(GetWorld(), Location, 15.F, 15, FColor::Red, true)
+#define DRAW_DEBUG_LINE(Start, End) if (GetWorld()) DrawDebugLine(GetWorld(), Start, End, FColor::Red, true)
+#define DRAW_DEBUG_POINT(Location) if(GetWorld()) DrawDebugPoint(GetWorld(), Location, 10, FColor::Red, true)
+#define DRAW_DEBUG_SPHERE_SingleFrame(Location) if (GetWorld()) DrawDebugSphere(GetWorld(), Location, 15.F, 15, FColor::Red, false, -1, 0)
+#define DRAW_DEBUG_LINE_SingleFrame(Start, End) if (GetWorld()) DrawDebugLine(GetWorld(), Start, End, FColor::Red, false, -1, 0)
+#define DRAW_DEBUG_POINT_SingleFrame(Location) if(GetWorld()) DrawDebugPoint(GetWorld(), Location, 10, FColor::Red, false, -1, 0)
+#define DRAW_DEBUG_VECTOR(Start, End) if (GetWorld()) \
+	{ \
+		DRAW_DEBUG_LINE(Start, End);\
+		DRAW_DEBUG_POINT(End);\
+	}
+
