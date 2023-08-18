bb150b2a70ad4da5ac69c8ecb636d0ef3e051a5c
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-08-18 18:31:43 +0300

    add main play AnimInstance class

---
+++ Source/EmpressOfBattlefield/Public/SlashAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "SlashAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EMPRESSOFBATTLEFIELD_API USlashAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
