3efc5090e9059706de1307bcaa8b0e32e7371167
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-08-23 17:49:06 +0300

    add Equip function to attach weapon to character

--- Source/EmpressOfBattlefield/Private/Weapon.cpp
+++ Source/EmpressOfBattlefield/Private/Weapon.cpp
@@ -3,6 +3,15 @@
 
 #include "Weapon.h"
 
+#include "SlashCharacter.h"
+
+void AWeapon::Equip(USceneComponent* InParent, FName InSocketName)
+{
+	IsItemTaken = true;
+	FAttachmentTransformRules AttachmentTransformRules(EAttachmentRule::SnapToTarget, true);
+	ItemMesh->AttachToComponent(InParent, AttachmentTransformRules, InSocketName);	
+}
+
 void AWeapon::OverlappingBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	Super::OverlappingBegin(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
--- Source/EmpressOfBattlefield/Public/Weapon.h
+++ Source/EmpressOfBattlefield/Public/Weapon.h
@@ -16,6 +16,7 @@ class EMPRESSOFBATTLEFIELD_API AWeapon : public AItem
 
 public:
 
+	void Equip(USceneComponent* InParent, FName InSocketName);
 	void OverlappingBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult) override;
 	void OverlappingEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
 };
