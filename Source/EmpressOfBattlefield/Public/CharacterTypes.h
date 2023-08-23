4e70a228a820646b3b6c14dc2cb2540e640fea51
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-08-24 02:04:07 +0300

    add character state enum header

---
+++ Source/EmpressOfBattlefield/Public/CharacterTypes.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+UENUM(Blueprintable)
+enum class ECharacterState
+{
+	ECS_Unequipped UMETA(Display = "Unequipped"),
+	ECS_EquippedOneHandWeapon UMETA(Display = "Equipped One-Hand Weapon"),
+	ECS_EquippedTwoHandWeapon UMETA(Display = "Equipped Two-Hand Weapon")
+};
