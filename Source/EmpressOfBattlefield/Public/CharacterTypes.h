a4b8c66c17512e2059d641f7949505662bc59647
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-09-02 18:59:09 +0300

    add action state enum

--- Source/EmpressOfBattlefield/Public/CharacterTypes.h
+++ Source/EmpressOfBattlefield/Public/CharacterTypes.h
@@ -1,9 +1,16 @@
 ﻿#pragma once
 
-UENUM(Blueprintable)
+UENUM(BlueprintType)
 enum class ECharacterState : uint8
 {
 	ECS_Unequipped UMETA(Display = "Unequipped"),
 	ECS_EquippedOneHandWeapon UMETA(Display = "Equipped One-Hand Weapon"),
 	ECS_EquippedTwoHandWeapon UMETA(Display = "Equipped Two-Hand Weapon")
+};
+
+UENUM(BlueprintType)
+enum class EActionState : uint8
+{
+	EAS_Unoccupied UMETA(Display = "Occupied"),
+	EAS_Attacking UMETA(Display = "Attacking")
 };
