c447b831531cc9dca7acdb18c880c74acf398a22
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-09-11 16:57:13 +0300

    add interface class to common methods to hit

---
+++ Source/EmpressOfBattlefield/Private/HitInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HitInterface.h"
+
+// Add default functionality here for any IHitInterface functions that are not pure virtual.
