8584712c533f9a978f3689e88cb87fa6e6d8a927
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-09-19 17:35:33 +0300

    add geometry collection module to use collection component

--- Source/EmpressOfBattlefield/EmpressOfBattlefield.Build.cs
+++ Source/EmpressOfBattlefield/EmpressOfBattlefield.Build.cs
@@ -8,7 +8,7 @@ public class EmpressOfBattlefield : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "HairStrandsCore", "Niagara"});
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "HairStrandsCore", "Niagara", "GeometryCollectionEngine"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
