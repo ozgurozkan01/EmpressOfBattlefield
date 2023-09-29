aaab536229aab2cc2ef71536a8a58853acbfe137
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-09-29 18:12:09 +0300

    add widget module

--- Source/EmpressOfBattlefield/EmpressOfBattlefield.Build.cs
+++ Source/EmpressOfBattlefield/EmpressOfBattlefield.Build.cs
@@ -8,7 +8,7 @@ public class EmpressOfBattlefield : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "HairStrandsCore", "Niagara", "GeometryCollectionEngine", "ChaosSolverEngine"});
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "HairStrandsCore", "Niagara", "GeometryCollectionEngine", "ChaosSolverEngine", "UMG"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
