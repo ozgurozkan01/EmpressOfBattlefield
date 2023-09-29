5fd5756e036c7f820a48a3773beab971eba96377
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-09-29 14:35:13 +0300

    add health bar widget class

---
+++ Source/EmpressOfBattlefield/Public/HealthBar.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "HealthBar.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EMPRESSOFBATTLEFIELD_API UHealthBar : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
