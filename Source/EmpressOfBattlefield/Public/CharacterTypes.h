06de8361dd3c4421de206512924ef5b3dc419a67
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-10-07 21:31:25 +0300

    add new character type enum

--- Source/EmpressOfBattlefield/Public/CharacterTypes.h
+++ Source/EmpressOfBattlefield/Public/CharacterTypes.h
@@ -15,3 +15,13 @@ enum class EActionState : uint8
 	EAS_Attacking UMETA(Display = "Attacking"),
 	EAS_EquipWeapon UMETA(Display = "Equip Weapon")
 };
+
+UENUM(BlueprintType)
+enum class EDeathPose : uint8
+{
+	EDP_Alive UMETA(Display = "Alive"),
+	EDP_Death1 UMETA(Display = "Death1"),
+	EDP_Death2 UMETA(Display = "Death2"),
+	EDP_Death3 UMETA(Display = "Death3"),
+	EDP_Death4 UMETA(Display = "Death4"),
+};
