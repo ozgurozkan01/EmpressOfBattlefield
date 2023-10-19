a762284024fc90c473f432c566fc22c0f6e9d101
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-10-19 09:54:35 +0300

    add IAModule to use AI control system

--- Source/EmpressOfBattlefield/EmpressOfBattlefield.Build.cs
+++ Source/EmpressOfBattlefield/EmpressOfBattlefield.Build.cs
@@ -8,7 +8,7 @@ public class EmpressOfBattlefield : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "HairStrandsCore", "Niagara", "GeometryCollectionEngine", "ChaosSolverEngine", "UMG"});
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "HairStrandsCore", "Niagara", "GeometryCollectionEngine", "ChaosSolverEngine", "UMG", "AIModule"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
