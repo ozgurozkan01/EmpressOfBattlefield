acc589e8bc3e0ed35b512b2749bf42633badc47e
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-11-16 11:29:56 +0300

    create slash overlay class to control user widget

---
+++ Source/EmpressOfBattlefield/Public/SlashOverlay.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SlashOverlay.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EMPRESSOFBATTLEFIELD_API USlashOverlay : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
