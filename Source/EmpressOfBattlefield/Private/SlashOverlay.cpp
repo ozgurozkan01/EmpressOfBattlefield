a9a36d5c792d3144ce3481747c7143802794cba5
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-11-17 10:50:01 +0300

    add slash user widget class to control health, stamina, gold and souls values

--- Source/EmpressOfBattlefield/Private/SlashOverlay.cpp
+++ Source/EmpressOfBattlefield/Private/SlashOverlay.cpp
@@ -3,3 +3,37 @@
 
 #include "SlashOverlay.h"
 
+#include "Components/ProgressBar.h"
+#include "Components/TextBlock.h"
+
+void USlashOverlay::SetHealthBarPercentage(float Percentage)
+{
+	if (HealthBar == nullptr) { return; }
+
+	HealthBar->SetPercent(Percentage);
+}
+
+void USlashOverlay::SetStaminaBarPercentage(float Percentage)
+{
+	if (StaminaBar == nullptr) { return; }
+
+	StaminaBar->SetPercent(Percentage);
+}
+
+void USlashOverlay::SetGoldCount(int32 Gold)
+{
+	if (GoldText == nullptr) { return; }
+
+	const FString GoldCountString = FString::Printf(TEXT("%d"), Gold);
+	const FText GoldCount = FText::FromString(GoldCountString);
+	GoldText->SetText(GoldCount);
+}
+
+void USlashOverlay::SetSoulsCount(int32 Souls)
+{
+	if (SoulsText == nullptr) { return; }
+
+	const FString SoulsCountString = FString::Printf(TEXT("%d"), Souls);
+	const FText SoulsCount = FText::FromString(SoulsCountString);
+	GoldText->SetText(SoulsCount);
+}
--- Source/EmpressOfBattlefield/Public/SlashOverlay.h
+++ Source/EmpressOfBattlefield/Public/SlashOverlay.h
@@ -6,12 +6,33 @@
 #include "Blueprint/UserWidget.h"
 #include "SlashOverlay.generated.h"
 
-/**
- * 
- */
+class UTextBlock;
+class UProgressBar;
+
 UCLASS()
 class EMPRESSOFBATTLEFIELD_API USlashOverlay : public UUserWidget
 {
 	GENERATED_BODY()
+
+public:
+
+	void SetHealthBarPercentage(float Percentage);
+	void SetStaminaBarPercentage(float Percentage);
+	void SetGoldCount(int32 Gold);
+	void SetSoulsCount(int32 Souls);
 	
+	
+private:
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UProgressBar> HealthBar;
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UProgressBar> StaminaBar;
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UTextBlock> GoldText;
+	
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UTextBlock> SoulsText;
 };
