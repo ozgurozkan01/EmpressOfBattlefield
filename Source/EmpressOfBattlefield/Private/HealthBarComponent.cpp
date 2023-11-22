2cb0d72de95146eab5902f6ff7526fb8533a7f0e
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-11-22 23:05:54 +0300

    refactor code delete debug messages

--- Source/EmpressOfBattlefield/Private/HealthBarComponent.cpp
+++ Source/EmpressOfBattlefield/Private/HealthBarComponent.cpp
@@ -1,15 +1,9 @@
 #include "HealthBarComponent.h"
-#include "HealthBar.h"
 #include "Components/ProgressBar.h"
-#include "Blueprint/UserWidget.h"
+#include "HealthBar.h"
 
 void UHealthBarComponent::SetHealthPercent(float Percent)
 {
-	if (!GetUserWidgetObject())
-	{
-		GEngine->AddOnScreenDebugMessage(1, 5, FColor::Cyan, TEXT("NO USER OBJECT"));
-	}
-	
 	if (HealthBarWidget == nullptr)
 	{
 		HealthBarWidget = Cast<UHealthBar>(GetUserWidgetObject());
